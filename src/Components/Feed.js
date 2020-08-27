778d736d07bd8f64a307f0eae9c87e766bcfa516
George Gardner <gpgardner@yahoo.com>
2020-08-27 16:54:30 -0400

    Starting to create stories

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './Feed.css'
+import StoryBoard from './StoryBoard'
 
 function Feed() {
 	return (
 		<div className='feed'>
 			
+
+			<StoryBoard />
+
+			{/* Create Post */}
+
+			{/* Posts */}
+
+
 		</div>
 	)
 }
