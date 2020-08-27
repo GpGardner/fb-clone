19d92dccf336773e9e8a007fef3e9689e891c1a0
George Gardner <gpgardner@yahoo.com>
2020-08-27 17:08:55 -0400

    Changing feed layout

--- src/Components/StoryCard.js
+++ src/Components/StoryCard.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import './StoryCard.css'
+
+
 import { Avatar } from '@material-ui/core'
+
 
 function StoryCard({image, profileSrc, title}) {
 
 
 	return (
-		<div className='storyCard'>
-			<img src={image} alt=""/>
-			<Avatar src={profileSrc}/>
+		<div style={{backgroundImage: `url(${image})`}} className='storyCard'>
+			<Avatar className="storyBoard-Avatar" src={profileSrc}/>
 			<h4>{title}</h4>
 		</div>
 	)
