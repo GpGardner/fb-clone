79d4d0d05244576e09ec09cec11e3cac51218607
George Gardner <gpgardner@yahoo.com>
2020-08-27 14:08:34 -0400

    adding firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAVoTNMi0ereJl8QnE400vjQHQv6szVKgY",
+  authDomain: "fb-clone-51661.firebaseapp.com",
+  databaseURL: "https://fb-clone-51661.firebaseio.com",
+  projectId: "fb-clone-51661",
+  storageBucket: "fb-clone-51661.appspot.com",
+  messagingSenderId: "964477703645",
+  appId: "1:964477703645:web:3349c5707a46da270c1234",
+  measurementId: "G-2MC1FSGH0R"
+};
