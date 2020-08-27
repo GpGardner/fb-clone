8196bb118e8fb21d65485ea1c61af9126372698f
George Gardner <gpgardner@yahoo.com>
2020-08-27 14:28:24 -0400

    connecting header css

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import "./Header.css"
 
 function Header() {
 	return (
 		<div className="header">
-			
+			<div className="header-left">
+				<img src="https://img.icons8.com/fluent/2x/facebook-new.png" alt="facebook icon"/>
+			</div>
+			<div className="header-middle">
+
+			</div>
+			<div className="header-right">
+
+			</div>
 		</div>
 	)
 }
