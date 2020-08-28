d3432f959999c181fa74791580d0aed860bebdc1
George Gardner <gpgardner@yahoo.com>
2020-08-28 15:07:10 -0400

    adding storyboard

--- src/Components/StoryBoard.js
+++ src/Components/StoryBoard.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import StoryCard from './StoryCard'
+import './StoryBoard.css'
 
 function StoryBoard() {
 	return (
 		<div className='storyBoard'>
+			<StoryCard title="hello" image='https://static01.nyt.com/images/2018/04/11/sports/11nhl-malkin/merlin_136379793_a40fa254-5a6e-4cd8-b8cd-3f6b9786eb09-superJumbo.jpg'/>
+			<StoryCard title="hello" image='https://static01.nyt.com/images/2018/04/11/sports/11nhl-malkin/merlin_136379793_a40fa254-5a6e-4cd8-b8cd-3f6b9786eb09-superJumbo.jpg'/>
+			<StoryCard title="hello" image='https://static01.nyt.com/images/2018/04/11/sports/11nhl-malkin/merlin_136379793_a40fa254-5a6e-4cd8-b8cd-3f6b9786eb09-superJumbo.jpg'/>
+			<StoryCard title="hello" image='https://static01.nyt.com/images/2018/04/11/sports/11nhl-malkin/merlin_136379793_a40fa254-5a6e-4cd8-b8cd-3f6b9786eb09-superJumbo.jpg'/>
 			<StoryCard title="hello" image='https://static01.nyt.com/images/2018/04/11/sports/11nhl-malkin/merlin_136379793_a40fa254-5a6e-4cd8-b8cd-3f6b9786eb09-superJumbo.jpg'/>
 		</div>
 	)
