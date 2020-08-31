b577e56d6ca370c61dda542604e8cc1d6dbf9042
George Gardner <gpgardner@yahoo.com>
2020-08-31 10:19:15 -0400

    Creating logic for inputs

--- src/Components/CreatePost.js
+++ src/Components/CreatePost.js
@@ -1,48 +1,55 @@
-import React from 'react'
-import "./CreatePost.css"
-import { Avatar } from '@material-ui/core'
-import VideoCamIcon from '@material-ui/icons/Videocam'
-import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary'
-import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import React, { useState } from "react";
+import "./CreatePost.css";
+import { Avatar } from "@material-ui/core";
+import VideoCamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 
 function CreatePost() {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+		console.log(`input: ${input}, image url: ${imageUrl}`)
+
+		// handle all of the DB logic
+		
+		setInput('')
+  };
 
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log('test');
-	}
-
-
-
-	return (
-		<div className='createPost'>
-			
-			<div className="createPost-top">
-				<Avatar />
-				<form onSubmit={handleSubmit}>
-					<input placeholder="What's on your mind?" className="createPost-input"/>
-					<input placeholder="Image URL" className="createPost-input"/>
-					<button className="createPost-button"></button>
-				</form>
-			</div>
-			<div className="createPost-bottom">
-				<div className="createPost-option">
-					<VideoCamIcon style={{ color: "red" }}/>
-					<h3>Live Video</h3>
-				</div>
-				<div className="createPost-option">
-					<PhotoLibraryIcon style={{ color: "green" }}/>
-					<h3>Live Video</h3>
-				</div>
-				<div className="createPost-option">
-					<InsertEmoticonIcon style={{ color: "yellow" }}/>
-					<h3>Live Video</h3>
-				</div>
-			</div>
-
-		</div>
-	)
+  return (
+    <div className="createPost">
+      <div className="createPost-top">
+        <Avatar />
+        <form onSubmit={handleSubmit}>
+          <input
+						value={input}
+						onChange={e => setInput(e.target.value)}
+            placeholder="What's on your mind?"
+            className="createPost-input"
+          />
+          <input value={imageUrl} onChange={e => setImageUrl(e.target.value)}placeholder="Image URL" className="createPost-input" />
+          <button className="createPost-button"></button>
+        </form>
+      </div>
+      <div className="createPost-bottom">
+        <div className="createPost-option">
+          <VideoCamIcon style={{ color: "red" }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className="createPost-option">
+          <PhotoLibraryIcon style={{ color: "green" }} />
+          <h3>Live Video</h3>
+        </div>
+        <div className="createPost-option">
+          <InsertEmoticonIcon style={{ color: "yellow" }} />
+          <h3>Live Video</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default CreatePost
+export default CreatePost;
