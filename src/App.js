8ac1b0c10fe2d2743c52fff37607b3a478bed436
George Gardner <gpgardner@yahoo.com>
2020-08-31 12:01:46 -0400

    Adding login feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import './App.css';
-import Header from './Components/Header';
-import Sidebar from './Components/Sidebar';
-import Feed from './Components/Feed';
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
+import Feed from "./Components/Feed";
+import Login from "./Components/Login";
 
 function App() {
+  const user = null;
   return (
     <div className="App">
-  
-      <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
 
-        <div className="app-body">
-          <Sidebar />
+          <div className="app-body">
 
-          <Feed />
+            <Sidebar />
 
-        </div>
+            <Feed />
 
+          </div>
 
-
+        </>
+      )}
     </div>
   );
 }
