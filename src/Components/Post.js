e7dae3614b576fa973ab11dfc98bfc4426228810
George Gardner <gpgardner@yahoo.com>
2020-08-31 12:02:57 -0400

    adding posts

---
+++ src/Components/Post.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Post.css'
+
+import Avatar from '@material-ui/core/Avatar'
+import ThumbUpIcon from "@material-ui/icons/ThumbUp"
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline"
+import NearMeIcon from "@material-ui/icons/NearMe"
+import ExpandMoreOutlined from "@material-ui/icons/ExpandMoreOutlined"
+import AccountCircleIcon from "@material-ui/icons/AccountCircle"
+
+
+
+function Post({profilePic, image, username, timestamp, message}) {
+	return (
+		<div className="post">
+			<div className="post-top">
+				<Avatar src={profilePic}
+				className="post-avatar"/>
+				<div className="post-topInfo">
+					<h3>{username}</h3>
+					<p>Timestamp...</p>
+				</div>
+			</div>
+
+			<div className="post-bottom">
+				<p>{message}</p>
+			</div>
+
+			<div className="post-image">
+				<img src={image} alt="users provided"/>
+			</div>
+			
+			<div className="post-options">
+				<div className="post-option">
+					<ThumbUpIcon />
+					<p>Like</p>
+				</div>
+				<div className="post-option">
+					<ChatBubbleOutlineIcon />
+					<p>Comment</p>
+				</div>
+				<div className="post-option">
+					<NearMeIcon />
+					<p>Share</p>
+				</div>
+				<div className="post-option">
+					<AccountCircleIcon />
+					<ExpandMoreOutlined />
+				</div>
+			</div>
+
+		</div>
+	)
+}
+
+export default Post
