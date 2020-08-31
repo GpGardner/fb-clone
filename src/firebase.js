2fada2804c87cc70395d691c7b725deae558a746
George Gardner <gpgardner@yahoo.com>
2020-08-31 12:01:56 -0400

    firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAVoTNMi0ereJl8QnE400vjQHQv6szVKgY",
   authDomain: "fb-clone-51661.firebaseapp.com",
@@ -8,3 +10,12 @@ const firebaseConfig = {
   appId: "1:964477703645:web:3349c5707a46da270c1234",
   measurementId: "G-2MC1FSGH0R"
 };
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export default db;
+export { auth, provider };
